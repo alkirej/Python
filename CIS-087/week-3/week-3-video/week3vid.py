2acbad75e12fb9a3bc94904534ca07b3d345b9cf
Jeff Alkire <jeff@alkire.net>
2022-09-02 23:42:57 -0700

    answered chapter questions

---
+++ CIS-087/week-3/week-3-video/week3vid.py
@@ -0,0 +1,12 @@
+names = ["Apple","Banana","Carrot","Dingbat"]
+names.append("Ernie")
+print ( "Banana" in names )
+
+n = range(5)
+print (n)
+
+for lcv in range(1, 12, 2):
+    print (lcv)
+
+print (3     +
+        5)
